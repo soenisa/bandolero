d6090c818577bf8b7f7cf041deee607319d0b802
Xavi Matos <matos.xav@gmail.com>
2016-11-06 16:51:50 -0500

    added chase behavior

---
+++ Assets/Jam Assets/Scripts/ChaseBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaseBehavior : MonoBehaviour
+{
+	bool chaseActivated = false;
+	public GameObject chasee;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!chaseActivated) {
+			var distance = gameObject.transform.position.x - chasee.transform.position.x;
+			Debug.Log(distance);
+			chaseActivated = distance < 10;
+		} else {
+			transform.position -= new Vector3(Time.deltaTime * 6, 0);
+		}
+	}
+}
