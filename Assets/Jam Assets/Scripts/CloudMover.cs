98d43944d5dce9266a65b7c7623ee8f15fe694ac
DESKTOP-32KAQPJ\ferra <g.kupfer@hotmail.com>
2016-11-06 13:22:58 -0500

    Added to scene

---
+++ Assets/Jam Assets/Scripts/CloudMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Walking ();
+
+	}
+
+	public void Walking (){
+		gameObject.transform.position += gameObject.transform.forward * Time.deltaTime * 8;
+	}
+}
